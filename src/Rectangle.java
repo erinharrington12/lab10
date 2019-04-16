380bd34f09f8f896061e631a393d6174d83f4d33
ErinHarrington <erin.c.harrington-1@ou.edu>
2019-04-16 18:27:07 -0500

    Finished Rectangle methods and test cases

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -3,8 +3,28 @@ import java.awt.Point;
 
 public class Rectangle extends Polygon
 {
+
+    /**
+     * 
+     * @param pointUL Upper left corner of the rectangle
+     * @param width   width of the rectangle
+     * @param height  height of the rectangle
+     * @param color   Desired color of the rectangle
+     * @param filled  Whether or not the rectangle should be filled
+     */
     public Rectangle(Point pointUL, int width, int height, Color color, boolean filled)
     {
+        super(color, filled);
+
+        location = new Point[4];
+
+        
+
+        // Assemble 4 points
+        location[0] = pointUL; // Upper left corner
+        location[1] = new Point(pointUL.x + width, pointUL.y); // Upper right corner
+        location[2] = new Point(pointUL.x, pointUL.y - height); // Lower left corner
+        location[3] = new Point(location[1].x, location[1].y - height); // Lower right corner
 
     }
 }
--- src/RectangleTest.java
+++ src/RectangleTest.java
@@ -1,6 +1,12 @@
-import static org.junit.Assert.*;
 
+
+import java.awt.Color;
+import java.awt.Point;
+
+import org.junit.Assert;
 import org.junit.Test;
+
+
 
 public class RectangleTest
 {
@@ -8,7 +14,29 @@ public class RectangleTest
     @Test
     public void testRectangle()
     {
-        fail("Not yet implemented");
+        Rectangle rect1 = new Rectangle(new Point(100,100), 50, 50, Color.BLACK, true);
+        
+        Point upperLeft1 = new Point(100,100);
+        Point upperRight1 = new Point(150, 100);
+        Point lowerLeft1 = new Point(100, 50);
+        Point lowerRight1 = new Point(150, 50);
+        
+        //Test if the points are there
+        
+        Point[] pts1 = rect1.getLocation();
+        Assert.assertEquals(4, pts1.length);
+        Assert.assertTrue(ShapeUtils.pointInSet(pts1, upperLeft1));
+        Assert.assertTrue(ShapeUtils.pointInSet(pts1, upperRight1));
+        Assert.assertTrue(ShapeUtils.pointInSet(pts1, lowerLeft1));
+        Assert.assertTrue(ShapeUtils.pointInSet(pts1, lowerRight1));
+       
+        //Verify if the rectangle is properly filled
+        Assert.assertTrue(rect1.isFilled());     
+        
+        //Verify if the diamond has the right color
+        
+        Color r1 = rect1.getColor();
+        Assert.assertEquals(Color.BLACK, r1);
     }
 
 }
