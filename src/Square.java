e044259f6a883d9d6a8e76536498dff5f1d3651c
ErinHarrington <erin.c.harrington-1@ou.edu>
2019-04-16 18:31:23 -0500

    Finished Square methods and test cases

--- src/Square.java
+++ src/Square.java
@@ -4,7 +4,15 @@ import java.awt.Point;
 public class Square extends Rectangle
 {
 
+    /**
+     * 
+     * @param pointUL Upper left corner of the square
+     * @param width Width (and height) of the square
+     * @param color Desired color for the square
+     * @param filled True if the square should be filled solid
+     */
     public Square(Point pointUL, int width, Color color, boolean filled) {
-        
+        super(pointUL, width, width, color, filled);
+       
     }
 }
--- src/SquareTest.java
+++ src/SquareTest.java
@@ -1,5 +1,9 @@
 import static org.junit.Assert.*;
 
+import java.awt.Color;
+import java.awt.Point;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 public class SquareTest
@@ -8,7 +12,29 @@ public class SquareTest
     @Test
     public void testSquare()
     {
-        fail("Not yet implemented");
+        Square sq1 = new Square(new Point(100, 100), 50, Color.BLACK, true);
+
+        Point upperLeft1 = new Point(100, 100);
+        Point upperRight1 = new Point(150, 100);
+        Point lowerLeft1 = new Point(100, 50);
+        Point lowerRight1 = new Point(150, 50);
+
+        // Test if the points are there
+
+        Point[] pts1 = sq1.getLocation();
+        Assert.assertEquals(4, pts1.length);
+        Assert.assertTrue(ShapeUtils.pointInSet(pts1, upperLeft1));
+        Assert.assertTrue(ShapeUtils.pointInSet(pts1, upperRight1));
+        Assert.assertTrue(ShapeUtils.pointInSet(pts1, lowerLeft1));
+        Assert.assertTrue(ShapeUtils.pointInSet(pts1, lowerRight1));
+
+        // Verify if the rectangle is properly filled
+        Assert.assertTrue(sq1.isFilled());
+
+        // Verify if the diamond has the right color
+
+        Color s1 = sq1.getColor();
+        Assert.assertEquals(Color.BLACK, s1);
     }
 
 }
